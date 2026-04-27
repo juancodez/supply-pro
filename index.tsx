ed82eaaa30562cced55bd91c2c8a2dab80170d0b
Juan Gomez Vara <95300952+juancodez@users.noreply.github.com>
2026-04-27 09:42:04 +0200

    Create index.tsx
    Fwp 01,02,03

---
+++ index.tsx
@@ -0,0 +1,326 @@
+import alertCircle from "./alert-circle.png";
+import exerciseGoal from "./exercise-goal.svg";
+import glyph from "./glyph.svg";
+import group from "./group.png";
+import group2 from "./group-2.png";
+import group3 from "./group-3.png";
+import group4 from "./group-4.png";
+import group5 from "./group-5.png";
+import group6 from "./group-6.png";
+import group7 from "./group-7.png";
+import group8 from "./group-8.png";
+import group9 from "./group-9.png";
+import group10 from "./group-10.png";
+import group11 from "./group-11.png";
+import group12 from "./group-12.png";
+import homescreenAppsIphone from "./homescreen-apps-iphone.png";
+import image from "./image.png";
+import ios14IphoneWallpaperIdownloadblogIspazioLight11 from "./ios-14-iphone-wallpaper-idownloadblog-ispazio-light-1-1.png";
+import moveGoal from "./move-goal.svg";
+import music1 from "./music-1.svg";
+import rectangle2 from "./rectangle-2.png";
+import standGoal from "./stand-goal.svg";
+import statusBarBattery from "./status-bar-battery.png";
+import statusBarService from "./status-bar-service.png";
+import statusBarWiFi from "./status-bar-wi-fi.png";
+
+const appIcons = [
+  {
+    id: "clock",
+    label: "Clock",
+    imageSrc: group,
+    className: "top-[calc(50.00%_-_348px)] left-[calc(50.00%_-_179px)]",
+    labelClassName: "w-[41.25%] top-[calc(50.00%_+_26px)] left-[30.00%]",
+  },
+  {
+    id: "tv",
+    label: "TV",
+    imageSrc: image,
+    className: "top-[calc(50.00%_-_249px)] left-[calc(50.00%_-_179px)]",
+    labelClassName: "w-[21.25%] top-[calc(50.00%_+_26px)] left-[40.00%]",
+  },
+  {
+    id: "camera",
+    label: "Camera",
+    imageSrc: group2,
+    className: "top-[calc(50.00%_-_348px)] left-[calc(50.00%_-_87px)]",
+    labelClassName: "w-[56.25%] top-[calc(50.00%_+_26px)] left-[22.50%]",
+  },
+  {
+    id: "stocks",
+    label: "Stocks",
+    imageSrc: group3,
+    className: "top-[calc(50.00%_-_249px)] left-[calc(50.00%_-_87px)]",
+    labelClassName: "w-[50.00%] top-[calc(50.00%_+_26px)] left-[25.00%]",
+  },
+  {
+    id: "health",
+    label: "Health",
+    imageSrc: group4,
+    className: "top-[calc(50.00%_+_47px)] left-[calc(50.00%_-_179px)]",
+    labelClassName: "w-[47.50%] top-[calc(50.00%_+_26px)] left-[26.25%]",
+  },
+  {
+    id: "news",
+    label: "News",
+    imageSrc: group5,
+    className: "top-[calc(50.00%_+_145px)] left-[calc(50.00%_-_179px)]",
+    labelClassName: "w-[40.00%] top-[calc(50.00%_+_26px)] left-[30.00%]",
+  },
+  {
+    id: "home",
+    label: "Home",
+    imageSrc: group6,
+    className: "top-[calc(50.00%_+_47px)] left-[calc(50.00%_-_87px)]",
+    labelClassName: "w-[42.50%] top-[calc(50.00%_+_26px)] left-[28.75%]",
+  },
+  {
+    id: "photos",
+    label: "Photos",
+    imageSrc: group7,
+    className: "top-[calc(50.00%_+_145px)] left-[calc(50.00%_-_87px)]",
+    labelClassName: "w-[51.25%] top-[calc(50.00%_+_26px)] left-[25.00%]",
+  },
+  {
+    id: "mail",
+    label: "Mail",
+    imageSrc: group8,
+    className: "top-[calc(50.00%_-_150px)] left-[calc(50.00%_+_3px)]",
+    labelClassName: "w-[30.00%] top-[calc(50.00%_+_26px)] left-[35.00%]",
+  },
+  {
+    id: "todoist-small",
+    label: "Todoist",
+    imageSrc: group10,
+    className: "top-[calc(50.00%_-_51px)] left-[calc(50.00%_+_3px)]",
+    labelClassName: "w-[53.75%] top-[calc(50.00%_+_26px)] left-[23.75%]",
+  },
+  {
+    id: "files",
+    label: "Files",
+    imageSrc: group11,
+    className: "top-[calc(50.00%_-_150px)] left-[calc(50.00%_+_95px)]",
+    labelClassName: "w-[33.75%] top-[calc(50.00%_+_26px)] left-[33.75%]",
+  },
+  {
+    id: "twist",
+    label: "Twist",
+    imageSrc: group12,
+    className: "top-[calc(50.00%_-_51px)] left-[calc(50.00%_+_95px)]",
+    labelClassName: "w-[38.75%] top-[calc(50.00%_+_26px)] left-[31.25%]",
+  },
+];
+
+export const Fwp = (): JSX.Element => {
+  return (
+    <main
+      className="relative min-h-[844px] min-w-[390px] w-full overflow-hidden"
+      aria-label="iPhone home screen mockup"
+    >
+      <section
+        className="absolute left-[calc(50.00%_-_195px)] top-0 flex h-[47px] w-[390px] justify-center bg-[#8d0a0a]"
+        aria-hidden="true"
+      >
+        <p className="-ml-px mt-3.5 flex h-[18px] w-[115px] items-center whitespace-nowrap [font-family:'Fira_Sans-SemiBold',Helvetica] text-xs font-semibold leading-[18px] tracking-[0.48px] text-white">
+          SAFE TO ZONE 47 PX
+        </p>
+      </section>
+      <section
+        className="absolute bottom-0 left-[calc(50.00%_-_195px)] h-[34px] w-[390px] bg-[#8d0a0a]"
+        aria-hidden="true"
+      >
+        <p className="absolute left-[calc(50.00%_-_74px)] top-[23.53%] flex h-[52.94%] items-center whitespace-nowrap [font-family:'Fira_Sans-SemiBold',Helvetica] text-xs font-semibold leading-[18px] tracking-[0.48px] text-white">
+          SAFE BOTTOM ZONE 37 PX
+        </p>
+      </section>
+      <section className="absolute left-[calc(50.00%_-_195px)] top-[calc(50.00%_-_422px)] h-[844px] w-[390px] aspect-[0.46]">
+        <div className="absolute left-0 top-0 flex h-[844px] w-[390px] bg-white">
+          <img
+            className="h-[844px] w-[390px] object-cover"
+            alt="iOS wallpaper background"
+            src={ios14IphoneWallpaperIdownloadblogIspazioLight11}
+          />
+        </div>
+        <header
+          className="absolute left-0 top-0 h-[46px] w-[390px]"
+          aria-label="Status bar"
+        >
+          <div className="absolute left-0 top-0 bg-black opacity-[0.0]" />
+          <img
+            className="absolute left-[89.07%] top-[29.55%] h-[70.45%] w-[10.93%]"
+            alt="Battery status"
+            src={statusBarBattery}
+          />
+          <img
+            className="absolute left-[77.87%] top-[29.55%] h-[70.45%] w-[22.13%]"
+            alt="Wi-Fi status"
+            src={statusBarWiFi}
+          />
+          <img
+            className="absolute left-[305px] top-[18px] h-[11px] w-[18px]"
+            alt="Cellular service status"
+            src={statusBarService}
+          />
+          <div
+            className="absolute left-[7.47%] top-[29.55%] h-[45.45%] w-[12.53%] overflow-hidden"
+            aria-label="Current time"
+          >
+            <div className="absolute left-[65.96%] top-[10.00%] h-[80.00%] w-[34.04%]" />
+            <div className="absolute left-[-2.13%] top-[calc(50.00%_-_11px)] w-[68.09%] whitespace-nowrap text-center [font-family:'SF_Pro_Text-Semibold',Helvetica] text-[15.6px] font-normal leading-[20.8px] tracking-[-0.25px] text-white">
+              9:41
+            </div>
+          </div>
+        </header>
+        <nav
+          className="absolute bottom-[11px] left-[2.93%] h-[98px] w-[94.67%]"
+          aria-label="Dock apps"
+        >
+          <div className="absolute -bottom-px left-0 h-[100px] w-[100.54%] rounded-[32.24px] bg-[#ffffff4c] backdrop-blur-[42.45px] backdrop-brightness-[100%] [-webkit-backdrop-filter:blur(42.45px)_brightness(100%)]" />
+          <img
+            className="absolute left-[4.51%] top-[13.26%] h-[86.74%] w-[95.49%]"
+            alt="Dock applications"
+            src={homescreenAppsIphone}
+          />
+        </nav>
+        <article
+          className="absolute left-[23px] top-[272px] h-[161px] w-[161px] overflow-hidden rounded-[22.54px] bg-[linear-gradient(180deg,rgba(219,75,61,1)_0%,rgba(208,55,40,1)_100%)]"
+          aria-label="Daily Goal widget"
+        >
+          <img
+            className="absolute left-[17px] top-20 h-[63px] w-[63px]"
+            alt="Daily goal progress"
+            src={group9}
+          />
+          <div className="absolute left-[17px] top-[30px] whitespace-nowrap [font-family:'SF_Pro_Text-Light',Helvetica] text-[33.3px] font-light leading-[normal] tracking-[0] text-white">
+            8/16
+          </div>
+          <h1 className="absolute left-[18px] top-[17px] [font-family:'SF_Pro_Text-Bold',Helvetica] text-[14.6px] font-bold leading-[normal] tracking-[0] text-white">
+            Daily Goal
+          </h1>
+          <img
+            className="absolute left-[24.52%] top-[61.94%] h-[38.06%] w-[75.48%]"
+            alt="Decorative glyph"
+            src={glyph}
+          />
+        </article>
+        <article
+          className="absolute left-[206px] top-[76px] flex h-[161px] w-[161px] overflow-hidden rounded-[22.54px] bg-[#121212]"
+          aria-label="Calendar activity widget"
+        >
+          <div className="ml-[16.6px] mt-[19.8px] flex h-[125.28px] w-[108px] flex-col gap-[9.4px]">
+            <div className="relative ml-[3.1px] h-[49.92px] w-[49.92px]">
+              <div className="absolute left-[-3px] top-[-3px] h-14 w-14 rounded-[28.08px] border-[6.24px] border-solid border-[#d02d2933]" />
+              <img
+                className="absolute left-[-3px] top-[-3px] h-14 w-14"
+                alt="Move goal ring"
+                src={moveGoal}
+              />
+              <div className="absolute left-[5px] top-[5px] h-10 w-10 rounded-[19.76px] border-[6.24px] border-solid border-[#6ed94033]" />
+              <img
+                className="absolute left-[5px] top-[5px] h-10 w-10"
+                alt="Exercise goal ring"
+                src={exerciseGoal}
+              />
+              <div className="absolute left-3.5 top-3.5 h-[23px] w-[23px] rounded-[11.44px] border-[6.24px] border-solid border-[#56bfdb33]" />
+              <img
+                className="absolute left-5 top-3.5 h-[23px] w-[17px]"
+                alt="Stand goal ring"
+                src={standGoal}
+              />
+            </div>
+            <p className="h-[66px] w-[106px] [font-family:'SF_Pro_Rounded-Regular',Helvetica] text-[18.7px] font-normal leading-[normal] tracking-[0] text-transparent">
+              <span className="text-[#d02d29]">350/475</span>
+              <span className="text-[#d02d29]">
+                CAL
+                <br />
+              </span>
+              <span className="text-[#6ed940]">15/30</span>
+              <span className="text-[#6ed940]">
+                min
+                <br />
+              </span>
+              <span className="text-[#56bfdb]">7/12</span>
+              <span className="text-[#56bfdb]">hrs</span>
+              <span className="[font-family:'SF_Pro_Rounded-Regular',Helvetica] text-[18.7px] font-normal tracking-[0] text-transparent">
+                AL
+              </span>
+            </p>
+          </div>
+        </article>
+        <article
+          className="absolute left-[206px] top-[469px] h-[161px] w-[161px] bg-[linear-gradient(180deg,rgba(30,86,144,0)_0%,rgba(14,85,156,0)_100%)]"
+          aria-label="Music widget"
+        >
+          <div className="absolute left-0 top-0 h-[161px] w-[161px] rounded-[22.54px] bg-white" />
+          <img
+            className="absolute left-[17px] top-[17px] h-[85px] w-[85px] object-cover"
+            alt="Album art"
+            src={rectangle2}
+          />
+          <div className="absolute left-[17px] top-[107px] whitespace-nowrap [font-family:'SF_Pro_Text-Semibold',Helvetica] text-[15.6px] font-normal leading-[normal] tracking-[0] text-black">
+            Cool
+          </div>
+          <div className="absolute left-[17px] top-[126px] whitespace-nowrap [font-family:'SF_Pro_Text-Regular',Helvetica] text-[15.6px] font-normal leading-[normal] tracking-[0] text-[#da0083]">
+            Dua Lipa
+          </div>
+          <img
+            className="absolute left-[124px] top-[17px] h-[25px] w-[21px]"
+            alt="Music icon"
+            src={music1}
+          />
+        </article>
+        <div className="absolute left-[20.80%] top-[calc(50.00%_+_15px)] w-[11.47%] whitespace-nowrap text-center [font-family:'SF_Pro_Text-Medium',Helvetica] text-[12.5px] font-medium leading-[normal] tracking-[0] text-white">
+          Todoist
+        </div>
+        <div className="absolute left-[66.40%] top-[calc(50.00%_-_180px)] w-[14.13%] whitespace-nowrap text-center [font-family:'SF_Pro_Text-Medium',Helvetica] text-[12.5px] font-medium leading-[normal] tracking-[0] text-white">
+          Calendar
+        </div>
+        <div className="absolute left-[68.80%] top-[calc(50.00%_+_213px)] w-[9.33%] whitespace-nowrap text-center [font-family:'SF_Pro_Text-Medium',Helvetica] text-[12.5px] font-medium leading-[normal] tracking-[0] text-white">
+          Music
+        </div>
+        {appIcons.map((app) => (
+          <div
+            key={app.id}
+            className={`absolute h-[82px] w-[83px] ${app.className}`}
+            aria-label={app.label}
+          >
+            <div
+              className={`absolute whitespace-nowrap text-center [font-family:'SF_Pro_Text-Medium',Helvetica] text-[12.5px] font-medium leading-[normal] tracking-[0] text-white ${app.labelClassName}`}
+            >
+              {app.label}
+            </div>
+            <img
+              className="absolute left-[12.50%] top-0 h-full w-[87.50%]"
+              alt={app.label}
+              src={app.imageSrc}
+            />
+          </div>
+        ))}
+      </section>
+      <div
+        className="absolute left-[calc(50.00%_-_179px)] top-[-102px] flex h-[51px] w-[359px] items-center justify-center gap-[7px] rounded-[10px] bg-red-600 px-[70px] pb-[13px] pt-3 opacity-0"
+        role="alert"
+        aria-live="polite"
+      >
+        <div className="relative ml-[-42.16px] h-[26px] w-[19.32px]">
+          <div className="absolute left-0 top-[29.35%] h-[70.65%] w-[70.34%] bg-[url(/vector.svg)] bg-[100%_100%]" />
+          <div className="absolute left-[57.24%] top-0 h-[46.13%] w-[42.76%]">
+            <div className="absolute left-0 top-0 h-full w-[38.71%] bg-white" />
+            <div className="absolute left-0 top-0 h-[64.44%] w-full rounded-[0.93px_4px_4px_0px] bg-white" />
+            <div className="absolute left-[38.71%] top-[18.89%] h-[26.67%] w-[25.81%] rounded-[0.13px_0.13px_0px_0px] bg-red-600" />
+          </div>
+        </div>
+        <div className="relative ml-[-15.84px] mr-[-42.16px] inline-flex flex-[0_0_auto] items-center justify-center gap-2.5">
+          <p className="relative flex w-fit items-end justify-center whitespace-nowrap font-fonts-fira-sans-font-body-MD-MOBILE text-center text-[length:var(--fonts-fira-sans-font-body-MD-MOBILE-font-size)] font-[number:var(--fonts-fira-sans-font-body-MD-MOBILE-font-weight)] leading-[var(--fonts-fira-sans-font-body-MD-MOBILE-line-height)] tracking-[var(--fonts-fira-sans-font-body-MD-MOBILE-letter-spacing)] text-white [font-style:var(--fonts-fira-sans-font-body-MD-MOBILE-font-style)]">
+            Die Zitronen sind nicht verfugbar.
+          </p>
+          <img
+            className="relative mt-[-5019.00px] h-6 w-6"
+            alt="Alert circle"
+            src={alertCircle}
+          />
+        </div>
+      </div>
+    </main>
+  );
+};
