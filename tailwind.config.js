add25930e692b502a1e620d6ac3bb4a78f58c794
Juan Gomez Vara <95300952+juancodez@users.noreply.github.com>
2026-04-27 09:42:36 +0200

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,110 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        "fonts-archivo-font-body-base-desktop":
+          "var(--fonts-archivo-font-body-base-desktop-font-family)",
+        "fonts-archivo-font-body-base-MOBILE":
+          "var(--fonts-archivo-font-body-base-MOBILE-font-family)",
+        "fonts-archivo-font-body-caption-MOBILE-TABLET":
+          "var(--fonts-archivo-font-body-caption-MOBILE-TABLET-font-family)",
+        "fonts-archivo-font-body-label-MOBILE":
+          "var(--fonts-archivo-font-body-label-MOBILE-font-family)",
+        "fonts-archivo-font-body-MD-desktop":
+          "var(--fonts-archivo-font-body-MD-desktop-font-family)",
+        "fonts-archivo-font-body-MD-MOBILE":
+          "var(--fonts-archivo-font-body-MD-MOBILE-font-family)",
+        "fonts-archivo-font-body-MD-tablet":
+          "var(--fonts-archivo-font-body-MD-tablet-font-family)",
+        "fonts-archivo-font-caption-desktop":
+          "var(--fonts-archivo-font-caption-desktop-font-family)",
+        "fonts-archivo-font-display-desktop":
+          "var(--fonts-archivo-font-display-desktop-font-family)",
+        "fonts-archivo-font-display-tablet":
+          "var(--fonts-archivo-font-display-tablet-font-family)",
+        "fonts-archivo-font-h1-bold-MOBILE":
+          "var(--fonts-archivo-font-h1-bold-MOBILE-font-family)",
+        "fonts-archivo-font-h1-desktop":
+          "var(--fonts-archivo-font-h1-desktop-font-family)",
+        "fonts-archivo-font-h1-tablet":
+          "var(--fonts-archivo-font-h1-tablet-font-family)",
+        "fonts-archivo-font-h2-desktop":
+          "var(--fonts-archivo-font-h2-desktop-font-family)",
+        "fonts-archivo-font-h2-semibold-MOBILE":
+          "var(--fonts-archivo-font-h2-semibold-MOBILE-font-family)",
+        "fonts-archivo-font-h2-tablet":
+          "var(--fonts-archivo-font-h2-tablet-font-family)",
+        "fonts-archivo-font-h3-desktop":
+          "var(--fonts-archivo-font-h3-desktop-font-family)",
+        "fonts-archivo-font-h3-semibold-MOBILE":
+          "var(--fonts-archivo-font-h3-semibold-MOBILE-font-family)",
+        "fonts-archivo-font-h3-tablet":
+          "var(--fonts-archivo-font-h3-tablet-font-family)",
+        "fonts-archivo-font-label":
+          "var(--fonts-archivo-font-label-font-family)",
+        "fonts-archivo-font-label-desktop":
+          "var(--fonts-archivo-font-label-desktop-font-family)",
+        "fonts-archivo-font-XXL-display-bold-MOBILE":
+          "var(--fonts-archivo-font-XXL-display-bold-MOBILE-font-family)",
+        "fonts-fira-sans-font-body-base-desktop":
+          "var(--fonts-fira-sans-font-body-base-desktop-font-family)",
+        "fonts-fira-sans-font-body-base-MOBILE":
+          "var(--fonts-fira-sans-font-body-base-MOBILE-font-family)",
+        "fonts-fira-sans-font-body-caption-MOBILE-TABLET":
+          "var(--fonts-fira-sans-font-body-caption-MOBILE-TABLET-font-family)",
+        "fonts-fira-sans-font-body-label-MOBILE":
+          "var(--fonts-fira-sans-font-body-label-MOBILE-font-family)",
+        "fonts-fira-sans-font-body-MD-desktop":
+          "var(--fonts-fira-sans-font-body-MD-desktop-font-family)",
+        "fonts-fira-sans-font-body-MD-MOBILE":
+          "var(--fonts-fira-sans-font-body-MD-MOBILE-font-family)",
+        "fonts-fira-sans-font-body-MD-tablet":
+          "var(--fonts-fira-sans-font-body-MD-tablet-font-family)",
+        "fonts-fira-sans-font-caption-desktop":
+          "var(--fonts-fira-sans-font-caption-desktop-font-family)",
+        "fonts-fira-sans-font-display-desktop":
+          "var(--fonts-fira-sans-font-display-desktop-font-family)",
+        "fonts-fira-sans-font-display-tablet":
+          "var(--fonts-fira-sans-font-display-tablet-font-family)",
+        "fonts-fira-sans-font-h1-bold-MOBILE":
+          "var(--fonts-fira-sans-font-h1-bold-MOBILE-font-family)",
+        "fonts-fira-sans-font-h1-desktop":
+          "var(--fonts-fira-sans-font-h1-desktop-font-family)",
+        "fonts-fira-sans-font-h1-tablet":
+          "var(--fonts-fira-sans-font-h1-tablet-font-family)",
+        "fonts-fira-sans-font-h2-desktop":
+          "var(--fonts-fira-sans-font-h2-desktop-font-family)",
+        "fonts-fira-sans-font-h2-semibold-MOBILE":
+          "var(--fonts-fira-sans-font-h2-semibold-MOBILE-font-family)",
+        "fonts-fira-sans-font-h2-tablet":
+          "var(--fonts-fira-sans-font-h2-tablet-font-family)",
+        "fonts-fira-sans-font-h3-desktop":
+          "var(--fonts-fira-sans-font-h3-desktop-font-family)",
+        "fonts-fira-sans-font-h3-semibold-MOBILE":
+          "var(--fonts-fira-sans-font-h3-semibold-MOBILE-font-family)",
+        "fonts-fira-sans-font-h3-tablet":
+          "var(--fonts-fira-sans-font-h3-tablet-font-family)",
+        "fonts-fira-sans-font-label":
+          "var(--fonts-fira-sans-font-label-font-family)",
+        "fonts-fira-sans-font-label-desktop":
+          "var(--fonts-fira-sans-font-label-desktop-font-family)",
+        "fonts-fira-sans-font-XXL-display-bold-MOBILE":
+          "var(--fonts-fira-sans-font-XXL-display-bold-MOBILE-font-family)",
+        "label-lowercase": "var(--label-lowercase-font-family)",
+      },
+      boxShadow: {
+        "brown-box-shadow": "var(--brown-box-shadow)",
+        "global-tokens-e0": "var(--global-tokens-e0)",
+        "global-tokens-e1": "var(--global-tokens-e1)",
+        "global-tokens-e2": "var(--global-tokens-e2)",
+        "global-tokens-e3": "var(--global-tokens-e3)",
+        "green-box-shadow": "var(--green-box-shadow)",
+        "green-menu-shadow-boxes": "var(--green-menu-shadow-boxes)",
+        "white-box-shadow": "var(--white-box-shadow)",
+      },
+    },
+  },
+  plugins: [],
+};
